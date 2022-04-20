b97bded310232ecb87e2c632e09876a87e35e147
EniyavanThangaraj <eniyavant405@gmail.com>
2022-04-20 22:37:48 +0530

    project structure

---
+++ src/org/blood/Blood.java
@@ -0,0 +1,90 @@
+package org.blood;
+
+public class Blood {
+
+public static void main(String[] args) {
+	
+	int age=1,weight=50;
+	if (age>=18) {
+		
+	  
+	if (weight>=50) {
+		System.out.println("able to donate the blood");
+	}
+	
+	else {
+
+	System.out.println("not able to donate the blood");
+	}
+	
+	}
+	else {
+		System.out.println("not able to donate the blood");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
